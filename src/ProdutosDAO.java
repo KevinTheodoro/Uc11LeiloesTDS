7334a455d46f53130e308642154e29ec8edfac40
Kevin Theodoro <kevinttsilva@outlook.com>
2023-08-25 12:32:31 -0300

    Implementado a listagem de produtos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -42,7 +42,27 @@ public class ProdutosDAO {
     }
 
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
+        String sql = "SELECT * FROM produtos";
+
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            while(resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+            }
+            
+        }catch(SQLException ex) {
+            
+        }
+
         return listagem;
     }
     
